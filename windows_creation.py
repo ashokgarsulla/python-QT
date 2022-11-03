287f1eb737cbfd1550abafaea7968afca591ac69
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-11-03 22:13:33 +0530

    Windows titttle changed

--- windows_creation.py
+++ windows_creation.py
@@ -10,7 +10,9 @@ app = QApplication([])
 
 # Create a Qt widget, which will be our window.
 window = QWidget()
+window.setWindowTitle("Showing Image....")
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
 
 # Start the event loop.
 app.exec()
